71b9721ab590d178700d28961613ace679e1e342
Özalp Döndüren <ozalp@donduren.com>
2019-01-20 02:15:20 +0300

    Added CameraFollow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+    public float speed = 100f;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        if (target != null)
+        {
+            offset = transform.position - target.transform.position;
+        }
+        else
+        {
+            Debug.LogError("Target was not set! Please a the target GameObject for to follow.");
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (target != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, offset + target.transform.position, speed * Time.deltaTime);
+        }
+    }
+}
