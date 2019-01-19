00cd32dbb83ade36b8353e7b0e4ddc1b1e8539ff
Özalp Döndüren <ozalp@donduren.com>
2019-01-19 15:53:21 +0300

    Added player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 500f;
+    public float horztSpeed = 20f;
+
+    private Rigidbody rbody;
+    private Camera cameraMain;
+
     private void Start()
     {
-
+        rbody = GetComponent<Rigidbody>();
+        cameraMain = Camera.main;
+        if (cameraMain == null)
+        {
+            Debug.LogError("Main camera could not find! Please set tag of a camera as 'MainCamera'.");
+        }
     }
 
     private void Update()
     {
-
+#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
+        inputH = Input.GetAxis("Horizontal");
+        xPos = Mathf.Clamp(transform.position.x + inputH, -2.5f, 2.5f);
+#endif
+        transform.position = Vector3.Lerp(transform.position, transform.position.With(x: xPos), horztSpeed);
+        rbody.velocity = Vector3.forward * speed * Time.deltaTime;
     }
+    float inputH, xPos;
 }
